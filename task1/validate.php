ceb0a778eb99367721e1f4cbdb6611e2be4f7a17
sadiaprodhan <55645529+sadiaprodhan@users.noreply.github.com>
2019-12-03 01:37:31 +0600

    new

---
+++ task1/validate.php
@@ -0,0 +1,18 @@
+<?php
+
+if(isset($_GET['id']))
+{$info=  $_GET['id'];
+echo ($info);
+/*$data= explode('$info','|');
+$date= $data[3]."/".data[4]."/".$data[5];
+		$conn = mysqli_connect('localhost', 'root', '','task1');
+		$sql = "insert into info values('{$data[0]}', '{$data[1]}','{$data[2]}', '{$date}', '{$data[6]}','{$data[7]}','{$data[8]}','{$data[9]}' )";
+		if(mysqli_query($conn, $sql)){
+		echo("successful");
+		
+		}
+		else
+		{echo("unsuccessful");}
+*/
+}
+?>
